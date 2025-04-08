d971e00aba73e5096f08f457d1ab311e73df27d7
Vasco <vascomyke@gmail.com>
2025-04-08 21:46:38 +0100

    Create App front page with description and buttons for functionalities

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>Apoio Local</h1>
+        <p>Esta aplicação web tem como objetivo ajudar pequenos negócios a crescer, fornecendo um espaço para promoção e recomendações personalizadas de marketing digital.</p>
+        <div className="button-group">
+          <button className="button">Login</button>
+          <button className="button">Registar</button>
+          <button className="button">Ver Estabelecimentos</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
