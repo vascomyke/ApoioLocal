c3d7d521ea75105cee3505ca2ab56e61acfc64d4
JoaoNBS04 <joaonbgs@gmail.com>
2025-06-17 02:00:23 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import HomePage from './HomePage';
-import LoginPage from './LoginPage';
-import RegisterPage from './RegisterPage';
+import PaginaInicial from './PaginaInicial';
+import PaginaLogin from './PaginaLogin';
+import PaginaRegisto from './PaginaRegisto';
 
 function App() {
   return (
     <Router>
       <nav style={{ padding: '10px', background: '#f0f0f0' }}>
         <Link to="/" style={{ marginRight: '10px' }}>Início</Link>
-        <Link to="/login" style={{ marginRight: '10px' }}>Login</Link>
-        <Link to="/register">Registar</Link>
+        <Link to="/login" style={{ marginRight: '10px' }}>Entrar</Link>
+        <Link to="/registar">Registar</Link>
       </nav>
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/" element={<PaginaInicial />} />
+        <Route path="/login" element={<PaginaLogin />} />
+        <Route path="/registar" element={<PaginaRegisto />} />
       </Routes>
     </Router>
   );
